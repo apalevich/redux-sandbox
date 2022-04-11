d5750710209e9d98e8f1f50e94038744a67ee3f3
Artyom Palevich <hello@apalevich.ru>
2022-04-11 16:18:32 +0300

    Refactoring

--- src/index.js
+++ src/index.js
@@ -1,37 +1,24 @@
 import { createStore } from "redux";
-
-const reducer = (state=0, action) => {
-    switch (action.type) {
-        case 'INC':
-            return state+1;
-        case 'DEC':
-            return state-1;
-        default:
-            return state
-    }
-}
-
-const anotherReducer = (state=0, action) => {
-    switch (action.type) {
-        case 'INC':
-            return state+10;
-        default:
-            return state
-    }
-}
+import reducer from "./reducer";
+import { inc, dec, rnd } from './actions';
 
 const store = createStore(reducer);
 
-const clickHandler = (id) => {
-    document
-        .getElementById(id)
-        .addEventListener('click', () => {
-            store.dispatch({type: id})
-        })
-}
-
-clickHandler('INC');
-clickHandler('DEC');
+document
+    .getElementById('INC')
+    .addEventListener('click', () => {
+        store.dispatch(inc())
+    })
+document
+    .getElementById('DEC')
+    .addEventListener('click', () => {
+        store.dispatch(dec())
+    })
+document
+    .getElementById('RND')
+    .addEventListener('click', () => {
+        store.dispatch(rnd(Math.ceil(Math.random()*10)))
+    })
 
 store.subscribe(() => {
     document.getElementById('counter').innerHTML = store.getState()
