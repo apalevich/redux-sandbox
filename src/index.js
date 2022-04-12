3bb45c51c7d19cc111c47670257f731c8318c693
Artyom Palevich <hello@apalevich.ru>
2022-04-12 12:00:07 +0300

    Switched to React

--- src/index.js
+++ src/index.js
@@ -1,25 +1,29 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
 import { createStore, bindActionCreators } from "redux";
 import reducer from "./reducer";
 import * as actions from './actions';
+import Counter from "./counter";
 
 const store = createStore(reducer);
 const { dispatch } = store;
 
 const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
 
-document
-    .getElementById('inc')
-    .addEventListener('click', inc)
-document
-    .getElementById('dec')
-    .addEventListener('click', dec)
-document
-    .getElementById('rnd')
-    .addEventListener('click', () => {
-        const val = Math.ceil(Math.random()*10);
-        rnd(val);
-    })
+const root = createRoot(document.getElementById('root'));
 
-store.subscribe(() => {
-    document.getElementById('counter').innerHTML = store.getState()
-})
+const update = () => {
+    root.render(<Counter 
+        counter={store.getState()}
+        inc={inc}
+        dec={dec}
+        rnd={() => {
+            const value = Math.ceil(Math.random() * 10);
+            return rnd(value);
+        }}/>)
+}
+
+update();
+store.subscribe(update);
+
